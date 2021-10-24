8c6fb6bb32002e126a55ab37f4c89bf836d1c5d2
Aleksandr Jakovchuk <aleksandr.jakovchuk@privatbank.ua>
2021-10-24 05:21:05 +0300

    hw3_array

--- src/com/pb/Yakovchuk/hw3/Array.java
+++ src/com/pb/Yakovchuk/hw3/Array.java
@@ -18,6 +18,22 @@ public class Array {
     sum = mas[0];
     for (int i=1; i< mas.length; i++)
         sum = sum + mas[i];
+    System.out.println("Массив");
+    for (int i=0; i< mas.length; i++)
+        System.out.print(mas[i]+" ");
+    int n = mas.length;
+        for (int i = 0; i < n - 1; i++)
+            for (int j = 0; j < n - i - 1; j++)
+                 if (mas[j] > mas[j+1]) {
+                     int temp = mas[j];
+                     mas[j] = mas[j+1];
+                     mas[j+1] = temp;
+                 }
+    System.out.println(" ");
+    System.out.println("Отсортированный массив");
+    for (int i=0; i< mas.length; i++)
+         System.out.print(mas[i]+" ");
+    System.out.println(" ");
     System.out.println("Максимальное число массива  " + max);
     System.out.println("Сумма элементов массива " + sum);
     }
