e665e5134f99cdbd037292e162ca13b18f886ca5
Aleksandr Jakovchuk <aleksandr.jakovchuk@privatbank.ua>
2021-11-23 23:02:22 +0200

    hw9

--- src/com/pb/Yakovchuk/hw9/FileNumbers.java
+++ src/com/pb/Yakovchuk/hw9/FileNumbers.java
@@ -1,6 +1,12 @@
 package com.pb.Yakovchuk.hw9;
 
 public class FileNumbers {
+    public static void creatNumbersFile() {
+
+    }
+    public static void creatOddNumbersFile() {
+
+    }
     public static void main(String[] args) {
 
     }
